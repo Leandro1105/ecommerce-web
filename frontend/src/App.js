9c539fb246d7ce238adcf9ed6e2da2d7e8516de2
Leandro Vieira <leandro.vieira@luizalabs.com>
2024-11-08 15:07:38 -0300

    Criação Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
